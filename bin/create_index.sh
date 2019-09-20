26fdec3da76ba9405fdb0693d357f1f1f3051e7f
Zafer Gurel <zafer.gurel@advancity.com.tr>
2019-09-20 11:18:15 +0300

    Initial scripts

---
+++ bin/create_index.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+op=$1
+folder=$2
+output_file=$3
+delta=${4:--30}
+
+if [ $op == "append" ] 
+then
+    find $folder -maxdepth 1 -type f -mtime $delta | xargs -P 16 -I @@ bash -c 'head -n 3 @@ | awk '\''/KEY: (.+)/{print "'@@'", $2}'\'' ' >> $output_file
+elif [ $op == "create" ]
+then
+    find $folder -maxdepth 1 -type f -mtime $delta | xargs -P 16 -I @@ bash -c 'head -n 3 @@ | awk '\''/KEY: (.+)/{print "'@@'", $2}'\'' ' > $output_file
+fi
---
+++ create_indexes.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+'''
+Creates index files for each nginx cache folder
+'''
+import os
+import subprocess
+import datetime
+import sys
+
+# Script parameters
+cache_folder = "/cache"
+index_folder = "/cache/_cacheindex"
+default_cache_duration = 30
+current_dir = os.getcwd()
+index_creator_script = current_dir + "/bin/create_index.sh"
+
+def create_indexes(op_mode="append"):
+    '''Creates index files for each nginx cache folder.
+    op_mode can be append or create
+    In "append" mode, index file creation dates are checked and only 
+    recently added cache files are added to index file.
+    In "create" mode index files are re-created.
+    Default mode is append.
+    '''
+    if op_mode != "append" and op_mode != "create":
+        op_mode = "append"
+    
+    if not os.path.exists(index_folder):
+        subprocess.Popen(["mkdir", "-p", index_folder]).wait()
+
+    folders = get_folders(cache_folder)
+    
+    for f in folders:
+        index_file = index_folder + "/" + f.replace("/", "_") + ".ind"
+        delta = -1 * default_cache_duration
+
+        if os.path.exists(index_file) and op_mode == "append":
+            # get creation time
+            ctime = datetime.datetime.fromtimestamp(os.stat(index_file).st_mtime)
+            delta = -1 * round((datetime.datetime.now() - ctime).seconds / 3600 / 24, 2)
+        
+        if delta < 0:
+            print("Cache operation: "+ op_mode + " -> " + f + " (" + str(delta) + " days)")
+            subprocess.Popen([index_creator_script, op_mode, f, index_file, str(delta)]).wait()
+
+def get_folders(base_folder):
+    '''Returns all the folders (except index_folder) under 
+    cache folder.
+    '''
+    folders = []
+    for root, dirs, _ in os.walk(base_folder):
+        for dir in dirs:
+            full_path = root + "/" + dir
+            only_files = [f for f in os.listdir(full_path) if os.path.isfile(os.path.join(full_path, f))]
+            if full_path != index_folder and len(only_files) > 0:
+                folders.append(full_path)
+    return folders
+
+
+if __name__ == "__main__":
+    op_mode = sys.argv[1] if len(sys.argv) > 1 else "append"
+    create_indexes(op_mode)
