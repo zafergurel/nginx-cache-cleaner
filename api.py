428d10f7c0a7c95da7938587e298c1309a47bf9b
Zafer Gurel <zafer.gurel@advancity.com.tr>
2019-09-25 15:26:48 +0300

    changed paths in form

--- api.py
+++ api.py
@@ -3,8 +3,10 @@ import os, signal, subprocess, argparse, time, re
 
 app = Flask(__name__)
 xstr = lambda s: s or ""
+current_dir = os.path.dirname(os.path.realpath(__file__))
+get_path = lambda p: os.path.join(current_dir, (p or ""))
 
-@app.route('/purge', methods=["POST"])
+@app.route('/cache-cleaner/purge', methods=["POST"])
 def purge():
     keyword = xstr(request.form.get("keyword"))
     cache_index_file = xstr(request.form.get("cache_index_file"))
@@ -14,7 +16,7 @@ def purge():
     elif cache_index_file == "perculus" and len(keyword) < 3:
         return Response("error: keyword-min-3-char", mimetype='text/plain')
 
-    output, error = subprocess.Popen(["bin/purge_cache.sh", keyword, cache_index_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    output, error = subprocess.Popen([get_path("bin/purge_cache.sh"), keyword, cache_index_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
     output_text = output.decode()
     output_text = str.strip(output_text)
@@ -24,7 +26,7 @@ def purge():
 
     return Response(output_text + "\n\n" + error_text, mimetype='text/plain')
 
-@app.route('/inspect', methods=["POST"])
+@app.route('/cache-cleaner/inspect', methods=["POST"])
 def inspect():
     keyword = xstr(request.form.get("keyword"))
     cache_index_file = xstr(request.form.get("cache_index_file"))
@@ -32,14 +34,16 @@ def inspect():
     if cache_index_file == '' or keyword == '':
         return Response("error: invalid-parameter", mimetype='text/plain')
 
-    output, error = subprocess.Popen(["bin/search_index.sh", keyword, cache_index_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    output, error = subprocess.Popen([get_path("bin/search_index.sh"), keyword, cache_index_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
     output_text = output.decode()
     error_text = error.decode if error else ""
 
     return Response(output_text + "\n\n" + error_text, mimetype='text/plain')
 
-@app.route('/cleaner')
+@app.route('/cache-cleaner/form')
+@app.route('/cache-cleaner/')
+@app.route('/cache-cleaner')
 def clean_cache_page():
     index_folder = "/cache/_cacheindex"
     index_files = []
--- bin/search_index.sh
+++ bin/search_index.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-grep -P "$1" $2 | awk '{print $1}'
+grep -P "$1" $2 -n
