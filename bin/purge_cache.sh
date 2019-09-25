5059b0bd67445d16c7b3dadd10c422e344cd9beb
Zafer Gurel <zafer.gurel@advancity.com.tr>
2019-09-25 14:37:13 +0300

    bugfix: purge_cache.sh now removes index entry as well

--- bin/purge_cache.sh
+++ bin/purge_cache.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-grep -P "$1" $2 | awk '{print $1}' | xargs rm
+pattern=$1
+index_file=$2
+# delete files in the cache directory
+grep -P "$pattern" "$index_file" | awk '{print $1}' | xargs rm
+
+# remove index entries from the index file
+grep -P "$pattern" "$index_file" -n | awk -F ":" '{print $1}' | xargs -I @@ sh -c "sed -i '"@@"c\*****' "$index_file
